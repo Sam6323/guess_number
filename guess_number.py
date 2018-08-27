c30233fdcd06c4479aa0a43982563ffd1304a794
Sam Chen <free6323@hotmail.com>
2018-08-27 22:55:00 +0800

    modify count part

--- guess_number.py
+++ guess_number.py
@@ -5,14 +5,12 @@ while True:
 	guess_num = int(input('請猜數字: '))
 	if guess_num == num:
 		print('終於猜對了!')
-		guess_time = guess_time + 1
+		guess_time += 1
 		print('你總共猜了', guess_time, '次')
 		break
 	elif guess_num > num:
 		print('你猜的數字比答案大')
-		guess_time = guess_time + 1
-		print('你已經猜了', guess_time, '次')
-	else:
+	elif guess_num < num:
 		print('你猜的數字比答案小')
-		guess_time = guess_time + 1
-		print('你已經猜了', guess_time, '次')
+	guess_time += 1
+	print('你已經猜了', guess_time, '次')
